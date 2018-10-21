1e3990e846b24d5b349b9e32989c666cd77bd329
devmjun <dev.mjun@gmail.com>
2018-10-21 22:08:22 +0900

    added Model for login

--- Explog/Explog/Sources/Model/LoginModel.swift
+++ Explog/Explog/Sources/Model/LoginModel.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+// MARK: LoginDataModel Set
+struct LoginModel: Codable {
+    var pk: Int
+    var username: String
+    var email: String
+    var imgProfile: String
+    var token: String
+    var apnsdeviceSet: [DeviceTokenDataModel]?
+    
+    enum CodingKeys: String, CodingKey {
+        case pk
+        case username
+        case email
+        case imgProfile = "img_profile"
+        case token
+        case apnsdeviceSet = "apnsdevice_set"
+    }
+}
+
+extension LoginModel {
+    struct DeviceTokenDataModel: Codable {
+        var deviceToken: String?
+        enum CodingKeys: String, CodingKey {
+            case deviceToken = "registration_id"
+        }
+    }
+}
+
+// MARK: Error Message DataModel
+extension LoginModel {
+    struct ErrorMessageDataModel: Codable {
+        var username: String?
+        var email: String?
+        var message: String?
+    }
+}
+
