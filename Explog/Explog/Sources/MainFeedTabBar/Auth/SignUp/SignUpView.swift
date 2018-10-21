40cce0b64605610762fd2d0ff3f767d8b5d011c4
devmjun <dev.mjun@gmail.com>
2018-10-21 22:10:22 +0900

    before developing

--- Explog/Explog/Sources/MainFeedTabBar/Auth/SignUp/SignUpView.swift
+++ Explog/Explog/Sources/MainFeedTabBar/Auth/SignUp/SignUpView.swift
@@ -7,14 +7,125 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
 final class SignUpView: BaseView<SignUpViewController> {
     
+    var containerScrollView = UIScrollView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    var contentView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    var darkBlurView = UIVisualEffectView().then {
+        $0.effect = UIBlurEffect(style: .dark)
+        $0.layer.opacity = 1.0
+    }
+    
+    var dismissButton = UIButton().then {
+        $0.backgroundColor = .clear
+        $0.setBackgroundImage(#imageLiteral(resourceName: "cancel"), for: .normal)
+        $0.layer.shouldRasterize = true // for iPhone SE
+    }
+    
+    var stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fillEqually
+        $0.spacing = 8
+    }
+    
+    var usernameTextField = SkyFloatingLabelTextField().then {
+        $0.placeholder = "Username"
+        $0.title = "Vaild Username"
+        $0.set(defaultColorStyle: .white)
+    }
+    
+    var emailTextField = SkyFloatingLabelTextField().then {
+        $0.placeholder = "Email"
+        $0.title = "Vaild Email"
+        $0.set(defaultColorStyle: .white)
+    }
+    
+    var passwordTextField = SkyFloatingLabelTextField().then {
+        $0.placeholder = "Password"
+        $0.title = "Vaild Password"
+        $0.isSecureTextEntry = true
+        $0.set(defaultColorStyle: .white)
+    }
+    
+    var signUpButton = ActivityIndicatorButton().then {
+        $0.setTitle("Sign up", for: [.normal, .highlighted])
+        $0.setTitleColor(.gray, highlightedStateColor: .blue)
+        $0.isUserInteractionEnabled = false
+        $0.layer.borderColor = UIColor.gray.cgColor
+        $0.layer.borderWidth = 2
+    }
+    
+    struct UI {
+        static var dismissButtonMargin: CGFloat = 6
+        static var stackViewleadingAndtrailingMargin: CGFloat = 6
+        static var stackViewTopMargin: CGFloat = UIScreen.main.bounds.height/7
+        static var stackViewHeight: CGFloat = UIScreen.main.bounds.height/4
+        static var loginBuggonTopMargin: CGFloat = 20
+    }
+    
     override func setupUI() {
+        backgroundColor = .clear
+        addSubviews([containerScrollView])
+        containerScrollView.addSubview(contentView)
+        contentView.addSubviews([darkBlurView, dismissButton, stackView, signUpButton])
+        stackView.addArrangedSubviews([usernameTextField, emailTextField, passwordTextField])
+        
+        containerScrollView
+            .topAnchor(to: topAnchor)
+            .bottomAnchor(to: bottomAnchor)
+            .leadingAnchor(to: leadingAnchor)
+            .trailingAnchor(to: trailingAnchor)
+            .activateAnchors()
+        
+        contentView
+            .topAnchor(to: containerScrollView.topAnchor)
+            .bottomAnchor(to: containerScrollView.bottomAnchor)
+            .leadingAnchor(to: containerScrollView.leadingAnchor)
+            .trailingAnchor(to: containerScrollView.trailingAnchor)
+            .dimensionAnchors(size: UIScreen.main.bounds.size)
+            .activateAnchors()
+        
+        darkBlurView
+            .topAnchor(to: contentView.topAnchor)
+            .bottomAnchor(to: contentView.bottomAnchor)
+            .leadingAnchor(to: contentView.leadingAnchor)
+            .trailingAnchor(to: contentView.trailingAnchor)
+            .activateAnchors()
+        
+        dismissButton
+            .topAnchor(to: contentView.layoutMarginsGuide.topAnchor, constant: UI.dismissButtonMargin)
+            .trailingAnchor(to: contentView.layoutMarginsGuide.trailingAnchor, constant: -UI.dismissButtonMargin)
+            .activateAnchors()
+        
+        stackView
+            .topAnchor(to: contentView.layoutMarginsGuide.topAnchor, constant: UI.stackViewTopMargin)
+            .heightAnchor(constant: UI.stackViewHeight)
+            .leadingAnchor(to: contentView.layoutMarginsGuide.leadingAnchor, constant: UI.stackViewleadingAndtrailingMargin)
+            .trailingAnchor(to: contentView.layoutMarginsGuide.trailingAnchor, constant: -UI.stackViewleadingAndtrailingMargin)
+            .activateAnchors()
+        
+        signUpButton
+            .topAnchor(to: stackView.bottomAnchor, constant: UI.loginBuggonTopMargin)
+            .leadingAnchor(to: stackView.leadingAnchor)
+            .trailingAnchor(to: stackView.trailingAnchor)
+            .heightAnchor(to: passwordTextField.heightAnchor)
+            .activateAnchors()
         
     }
     
     override func setupBinding() {
-        
+        dismissButton.addTarget(vc, action: #selector(vc.dismissButtonAction(_:)), for: .touchUpInside)
+        usernameTextField.addTarget(vc, action: #selector(vc.textFieldDidChange(_:)), for: .editingChanged)
+        emailTextField.addTarget(vc, action: #selector(vc.textFieldDidChange(_:)), for: .editingChanged)
+        passwordTextField.addTarget(vc, action: #selector(vc.textFieldDidChange(_:)), for: .editingChanged)
+        signUpButton.addTarget(vc, action: #selector(vc.signUpButtonAction(_:)), for: .touchUpInside)
     }
 }
--- Explog/Explog/Sources/MainFeedTabBar/Auth/SignUp/SignUpViewController.swift
+++ Explog/Explog/Sources/MainFeedTabBar/Auth/SignUp/SignUpViewController.swift
@@ -7,7 +7,26 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
 final class SignUpViewController: BaseViewController {
+    lazy var v = SignUpView(controlBy: self)
     
+    override func loadView() {
+        super.loadView()
+        view = v
+    }
+    
+    @objc func dismissButtonAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func textFieldDidChange(_ textField: SkyFloatingLabelTextField) {
+        
+    }
+    
+    @objc func signUpButtonAction(_ sender: ActivityIndicatorButton) {
+        
+    }
 }
+
