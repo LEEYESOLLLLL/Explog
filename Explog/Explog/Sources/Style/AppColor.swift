b56fd05bb1ffbb134679fb0c6f819d403d416de7
devmjun <dev.mjun@gmail.com>
2018-11-06 14:34:31 +0900

    modified app logo

--- Explog/Explog/Sources/Style/AppColor.swift
+++ Explog/Explog/Sources/Style/AppColor.swift
@@ -10,6 +10,8 @@ import UIKit
 
 extension UIColor {
     static var appStyle: UIColor {
-        return UIColor(red:0.36, green:0.63, blue:0.95, alpha:1.00)
+//        return UIColor(red:0.36, green:0.63, blue:0.95, alpha:1.00)
+        return UIColor(red:0.45, green:0.97, blue:0.63, alpha:1.00)
     }
+    
 }
