0831a1933e4d245c24752efc289342e39de1e4f3
devmjun <dev.mjun@gmail.com>
2018-11-23 18:14:55 +0900

    Setup initialization

--- Explog/Explog/Sources/MainFeedTabBar/Search/SearchState.swift
+++ Explog/Explog/Sources/MainFeedTabBar/Search/SearchState.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+/*
+ SearchViewController에 필요한 상태는..
+ loading, populated, pagenation, error, empty
+ // 이거하기전에, 여러개의 컴퓨터에서 인증서 관리쉽게할수 있는것 찾아서 설치해놓자, 인증서 떄문에 귀찮아 지지 않게..
+ */
 extension SearchViewController {
     enum State {
         case loading
